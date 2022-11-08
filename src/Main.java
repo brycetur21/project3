07488178445870b69f7e8e8829e424f90ebe3535
Bryce T <brycetur21@gmail.com>
2022-11-08 14:34:43 -0600

    update

--- src/Main.java
+++ src/Main.java
@@ -7,7 +7,7 @@ public class Main {
         String current_algorithm = null;
 
         int number_of_cores = 1; // default number of cores
-        int time_quantum = 0;
+        int time_quantum = 0; // time quantum for round robin
 
         int num_args = args.length;
 
@@ -17,18 +17,27 @@ public class Main {
 
             switch (args[0]) {
                 case "-S" -> {
-                    int algorithm_num = Integer.parseInt(args[1]);
+                    int algorithm_num = 0;
+
+                    // parse the algorithm number as long as it is not a char
+                    try {
+                        algorithm_num = Integer.parseInt(args[1]);
+                    } catch (NumberFormatException ex) {
+                        System.out.println("char entered instead of number");
+                    }
+
                     if (algorithm_num > 0 && algorithm_num < 5 && algorithm_num != 2)
                         current_algorithm = algorithms[algorithm_num - 1];
-                    else
-                        System.out.println("invalid algorithm number");
                 }
                 case "-C" -> {
-                    number_of_cores = Integer.parseInt(args[0]);
-                    if (number_of_cores < 1 || number_of_cores > 4)
-                        System.out.println("invalid number of cores");
+
+                    // parse the number of cores as long as it is not a char
+                    try {
+                        number_of_cores = Integer.parseInt(args[0]);
+                    } catch (NumberFormatException ex) {
+                        System.out.println("char entered instead of number");
+                    }
                 }
-                default -> System.out.println("invalid input");
             }
         }
         // case where we have "-S 2 <num>"
@@ -36,13 +45,14 @@ public class Main {
 
             if (Objects.equals(args[0], "-S") && Objects.equals(args[1], "2")) {
                 current_algorithm = algorithms[1];
-                time_quantum = Integer.parseInt(args[2]);
 
-                if (time_quantum < 1)
-                    System.out.println("time quantum invalid");
+                // parse the time quantum as long as it is not a char
+                try {
+                    time_quantum = Integer.parseInt(args[2]);
+                } catch (NumberFormatException ex) {
+                    System.out.println("char entered instead of number");
+                }
             }
-            else
-                System.out.println("invalid input");
         }
         // case where we have "-S 1 -C 2" or "-C 2 -S 1"
         else if (num_args == 4) {
@@ -50,86 +60,131 @@ public class Main {
             // first two arguments
             if (Objects.equals(args[0], "-S") || Objects.equals(args[0], "-C")) {
                 if (args[0].equals("-S")) {
-                    int algorithm_num = Integer.parseInt(args[1]);
+                    int algorithm_num = 0;
+
+                    // parse algorithm number as long as it is not a char
+                    try{
+                        algorithm_num = Integer.parseInt(args[1]);
+                    } catch (NumberFormatException ex) {
+                        System.out.println("char entered instead of number");
+                    }
 
                     if (algorithm_num > 0 && algorithm_num < 5 && algorithm_num != 2)
                         current_algorithm = algorithms[algorithm_num-1];
-                    else
-                        System.out.println("invalid algorithm number");
                 }
                 else if (args[0].equals("-C")) {
-                    number_of_cores = Integer.parseInt(args[1]);
 
-                    if (number_of_cores < 1 || number_of_cores > 4)
-                        System.out.println("invalid number of cores");
+                    // parse the number of cores as long as it is not a char
+                    try {
+                        number_of_cores = Integer.parseInt(args[0]);
+                    } catch (NumberFormatException ex) {
+                        System.out.println("char entered instead of number");
+                    }
                 }
             }
-            else
-                System.out.println("invalid input");
 
             // second two arguments
             if (Objects.equals(args[2], "-S") || Objects.equals(args[2], "-C")) {
                 if (args[2].equals("-S")) {
-                    int algorithm_num = Integer.parseInt(args[3]);
+                    int algorithm_num = 0;
+
+                    // parse algorithm number as long as it is not a char
+                    try{
+                        algorithm_num = Integer.parseInt(args[1]);
+                    } catch (NumberFormatException ex) {
+                        System.out.println("char entered instead of number");
+                    }
 
                     if (algorithm_num > 0 && algorithm_num < 5 && algorithm_num != 2)
                         current_algorithm = algorithms[algorithm_num-1];
-                    else
-                        System.out.println("invalid algorithm number");
+
                 }
                 else if (args[2].equals("-C")) {
-                    number_of_cores = Integer.parseInt(args[3]);
 
-                    if (number_of_cores < 1 || number_of_cores > 4)
-                        System.out.println("invalid number of cores");
+                    // parse the number of cores as long as it is not a char
+                    try {
+                        number_of_cores = Integer.parseInt(args[0]);
+                    } catch (NumberFormatException ex) {
+                        System.out.println("char entered instead of number");
+                    }
                 }
             }
-            else
-                System.out.println("invalid input");
         }
         // case were we have "-S 2 <num> -C 2" or "-C 2 -S 2 <num>"
         else if (num_args == 5) {
 
-            if (Objects.equals(args[0], "-S") || Objects.equals(args[0], "-C")) {
-                if (args[0].equals("-S") && args[1].equals("2")) {
+            if (Objects.equals(args[0], "-S") && Objects.equals(args[3], "-C")) {
+                int algorithm_num = 0;
+
+                // parse algorithm number as long as it is not a char
+                try {
+                    algorithm_num = Integer.parseInt(args[1]);
+                } catch (NumberFormatException ex) {
+                    System.out.println("char entered instead of number");
+                }
+
+                // check that the algorithm number is 2
+                if (algorithm_num == 2)
                     current_algorithm = algorithms[1];
+
+                // parse time quantum as long as it is not a char
+                try {
                     time_quantum = Integer.parseInt(args[2]);
-
-                    if (time_quantum < 1)
-                        System.out.println("time quantum invalid");
+                } catch (NumberFormatException ex) {
+                    System.out.println("char entered instead of number");
                 }
-                else if (args[0].equals("-C")) {
-                    number_of_cores = Integer.parseInt(args[2]);
 
-                    if (number_of_cores < 1 || number_of_cores > 4)
-                        System.out.println("invalid number of cores");
-                }
-            }
-            else
-                System.out.println("invalid input");
-
-            if (Objects.equals(args[2], "-S") || Objects.equals(args[3], "-C")) {
-                if (args[2].equals("-S") && args[3].equals("2")) {
-                    time_quantum = Integer.parseInt(args[2]);
-
-                    if (time_quantum < 1)
-                        System.out.println("time quantum invalid");
-                }
-                else if (args[3].equals("-C")) {
+                // parse the number of cores as long as it is not a char
+                try {
                     number_of_cores = Integer.parseInt(args[4]);
-
-                    if (number_of_cores < 1 || number_of_cores > 4)
-                        System.out.println("invalid number of cores");
+                } catch (NumberFormatException ex) {
+                    System.out.println("char entered instead of a number");
                 }
             }
-            else
-                System.out.println("invalid input");
-        }
-        else
-            System.out.println("invalid input");
 
-        System.out.println("Algorithm: " + current_algorithm);
-        System.out.println("Time Quantum: " + time_quantum);
-        System.out.println("Number of cores: " + number_of_cores);
+            if (Objects.equals(args[0], "-C") || Objects.equals(args[2], "-S")) {
+                int algorithm_num = 0;
+
+                // parse algorithm number as long as it is not a char
+                try {
+                    algorithm_num = Integer.parseInt(args[3]);
+                } catch (NumberFormatException ex) {
+                    System.out.println("char entered instead of number");
+                }
+
+                // check that the algorithm number is 2
+                if (algorithm_num == 2)
+                    current_algorithm = algorithms[1];
+
+                // parse time quantum as long as it is not a char
+                try {
+                    time_quantum = Integer.parseInt(args[4]);
+                } catch (NumberFormatException ex) {
+                    System.out.println("char entered instead of number");
+                }
+
+                // parse the number of cores as long as it is not a char
+                try {
+                    number_of_cores = Integer.parseInt(args[1]);
+                } catch (NumberFormatException ex) {
+                    System.out.println("char entered instead of a number");
+                }
+            }
+        }
+
+        System.out.println("Algorithm: " + current_algorithm); // algorithm produces null if invalid
+
+        // check that the number of cores is greater than 0, but less than 5
+        if (number_of_cores < 1 || number_of_cores > 4)
+            System.out.println("number of cores is invalid");
+        else
+            System.out.println("Number of cores: " + number_of_cores);
+
+        // check that the time quantum is 0 or greater and is bounded to 100
+        if (time_quantum < 0 || time_quantum > 100)
+            System.out.println("Time quantum is invalid");
+        else
+            System.out.println("time quantum: " + time_quantum);
+
     }
 }
