447d0a5fff4d7fccaa78e740250f41279975ce16
Bryce T <brycetur21@gmail.com>
2022-11-08 22:49:52 -0600

    update

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,11 @@
 import java.util.Objects;
 
 public class Main {
-    public static void main(String[] args) {
 
+    // this function takes care of checking the input arguments for validity as entered
+    public static String[] argument_checker(String[] args) {
+
+        // possible algorithms
         String[] algorithms = {"FCFS", "RR", "NPSJF", "PSJF"};
         String current_algorithm = null;
 
@@ -10,9 +13,10 @@ public class Main {
         int time_quantum = 0; // time quantum for round robin
 
         int num_args = args.length;
+        int invalid = 0; // variable to keep track of valid input
 
         // case where we have "-S <num>" or "-C <num>"
-        // note that "-S 2" is not possible here
+        // note that "-S 2" is not possible here because we need a time quantum
         if (num_args == 2) {
 
             switch (args[0]) {
@@ -23,7 +27,7 @@ public class Main {
                     try {
                         algorithm_num = Integer.parseInt(args[1]);
                     } catch (NumberFormatException ex) {
-                        System.out.println("char entered instead of number");
+                        invalid = 1;
                     }
 
                     if (algorithm_num > 0 && algorithm_num < 5 && algorithm_num != 2)
@@ -35,7 +39,7 @@ public class Main {
                     try {
                         number_of_cores = Integer.parseInt(args[0]);
                     } catch (NumberFormatException ex) {
-                        System.out.println("char entered instead of number");
+                        invalid = 1;
                     }
                 }
             }
@@ -50,7 +54,7 @@ public class Main {
                 try {
                     time_quantum = Integer.parseInt(args[2]);
                 } catch (NumberFormatException ex) {
-                    System.out.println("char entered instead of number");
+                    invalid = 1;
                 }
             }
         }
@@ -58,55 +62,46 @@ public class Main {
         else if (num_args == 4) {
 
             // first two arguments
-            if (Objects.equals(args[0], "-S") || Objects.equals(args[0], "-C")) {
-                if (args[0].equals("-S")) {
-                    int algorithm_num = 0;
+            if (Objects.equals(args[0], "-S") && Objects.equals(args[2], "-C")) {
+                int algorithm_num = 0;
 
-                    // parse algorithm number as long as it is not a char
-                    try{
-                        algorithm_num = Integer.parseInt(args[1]);
-                    } catch (NumberFormatException ex) {
-                        System.out.println("char entered instead of number");
-                    }
-
-                    if (algorithm_num > 0 && algorithm_num < 5 && algorithm_num != 2)
-                        current_algorithm = algorithms[algorithm_num-1];
+                // parse algorithm as long as it is not a char
+                try {
+                    algorithm_num = Integer.parseInt(args[1]);
+                } catch (NumberFormatException ex) {
+                    invalid = 1;
                 }
-                else if (args[0].equals("-C")) {
 
-                    // parse the number of cores as long as it is not a char
-                    try {
-                        number_of_cores = Integer.parseInt(args[0]);
-                    } catch (NumberFormatException ex) {
-                        System.out.println("char entered instead of number");
-                    }
+                if (algorithm_num > 0 && algorithm_num < 5 && algorithm_num != 2)
+                    current_algorithm = algorithms[algorithm_num-1];
+
+                // parse number of cores as long as it is not a char
+                try {
+                    number_of_cores = Integer.parseInt(args[3]);
+                } catch (NumberFormatException ex) {
+                    invalid = 1;
                 }
             }
 
             // second two arguments
-            if (Objects.equals(args[2], "-S") || Objects.equals(args[2], "-C")) {
-                if (args[2].equals("-S")) {
-                    int algorithm_num = 0;
+            if (Objects.equals(args[2], "-S") || Objects.equals(args[0], "-C")) {
+                int algorithm_num = 0;
 
-                    // parse algorithm number as long as it is not a char
-                    try{
-                        algorithm_num = Integer.parseInt(args[1]);
-                    } catch (NumberFormatException ex) {
-                        System.out.println("char entered instead of number");
-                    }
-
-                    if (algorithm_num > 0 && algorithm_num < 5 && algorithm_num != 2)
-                        current_algorithm = algorithms[algorithm_num-1];
-
+                // parse algorithm as long as it is not a char
+                try {
+                    algorithm_num = Integer.parseInt(args[3]);
+                } catch (NumberFormatException ex) {
+                    invalid = 1;
                 }
-                else if (args[2].equals("-C")) {
 
-                    // parse the number of cores as long as it is not a char
-                    try {
-                        number_of_cores = Integer.parseInt(args[0]);
-                    } catch (NumberFormatException ex) {
-                        System.out.println("char entered instead of number");
-                    }
+                if (algorithm_num > 0 && algorithm_num < 5 && algorithm_num != 2)
+                    current_algorithm = algorithms[algorithm_num-1];
+
+                // parse number of cores as long as it is not a char
+                try {
+                    number_of_cores = Integer.parseInt(args[1]);
+                } catch (NumberFormatException ex) {
+                    invalid = 1;
                 }
             }
         }
@@ -120,7 +115,7 @@ public class Main {
                 try {
                     algorithm_num = Integer.parseInt(args[1]);
                 } catch (NumberFormatException ex) {
-                    System.out.println("char entered instead of number");
+                    invalid = 1;
                 }
 
                 // check that the algorithm number is 2
@@ -131,14 +126,14 @@ public class Main {
                 try {
                     time_quantum = Integer.parseInt(args[2]);
                 } catch (NumberFormatException ex) {
-                    System.out.println("char entered instead of number");
+                    invalid = 1;
                 }
 
                 // parse the number of cores as long as it is not a char
                 try {
                     number_of_cores = Integer.parseInt(args[4]);
                 } catch (NumberFormatException ex) {
-                    System.out.println("char entered instead of a number");
+                    invalid = 1;
                 }
             }
 
@@ -149,7 +144,7 @@ public class Main {
                 try {
                     algorithm_num = Integer.parseInt(args[3]);
                 } catch (NumberFormatException ex) {
-                    System.out.println("char entered instead of number");
+                    invalid = 1;
                 }
 
                 // check that the algorithm number is 2
@@ -160,31 +155,56 @@ public class Main {
                 try {
                     time_quantum = Integer.parseInt(args[4]);
                 } catch (NumberFormatException ex) {
-                    System.out.println("char entered instead of number");
+                    invalid = 1;
                 }
 
                 // parse the number of cores as long as it is not a char
                 try {
                     number_of_cores = Integer.parseInt(args[1]);
                 } catch (NumberFormatException ex) {
-                    System.out.println("char entered instead of a number");
+                    invalid = 1;
                 }
             }
         }
 
-        System.out.println("Algorithm: " + current_algorithm); // algorithm produces null if invalid
+        if (invalid == 0)
+            return new String[]{current_algorithm, Integer.toString(time_quantum), Integer.toString(number_of_cores)};
+        return null;
+    }
 
-        // check that the number of cores is greater than 0, but less than 5
-        if (number_of_cores < 1 || number_of_cores > 4)
-            System.out.println("number of cores is invalid");
+    public static void main(String[] args) {
+
+        String algorithm;
+        int time_quantum;
+        int number_of_cores;
+
+        String[] parameters = argument_checker(args);
+        if (parameters != null) {
+            algorithm = parameters[0];
+            time_quantum = Integer.parseInt(parameters[1]);
+            number_of_cores = Integer.parseInt(parameters[2]);
+
+            // algorithm validity
+            if (algorithm != null)
+                System.out.println("algorithm: " + algorithm);
+            else
+                System.out.println("algorithm is invalid");
+
+            // number of cores validity
+            if (number_of_cores < 1 || number_of_cores > 4)
+                System.out.println("number of cores is invalid");
+            else
+                System.out.println("Number of cores: " + number_of_cores);
+
+            // time quantum validity
+            if (time_quantum < 0 || time_quantum > 100)
+                System.out.println("Time quantum is invalid");
+            else if (time_quantum < 1 && Objects.equals(algorithm, "RR"))
+                System.out.println("time quantum must be greater than 0 for RR");
+            else
+                System.out.println("time quantum: " + time_quantum);
+        }
         else
-            System.out.println("Number of cores: " + number_of_cores);
-
-        // check that the time quantum is 0 or greater and is bounded to 100
-        if (time_quantum < 0 || time_quantum > 100)
-            System.out.println("Time quantum is invalid");
-        else
-            System.out.println("time quantum: " + time_quantum);
-
+            System.out.println("invalid input, check input parameters");
     }
 }
